4ab783a0cd2265a2663bb3475e7897482b3a0020
Kyle-Law <kylelaw1121@gmail.com>
2022-06-21 17:05:59 +0800

    Fix print error from GetEnvVars function

--- env_vars.go
+++ env_vars.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-// GetEnvVars - /environments - Returns list of environment_variables under the accout
+// GetEnvVars - /environments - Returns list of environment_variables under the account
 func (c *Client) GetEnvVars() ([]EnvVar, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/environment_variables", c.HostURL), nil)
 	if err != nil {
@@ -17,9 +17,6 @@ func (c *Client) GetEnvVars() ([]EnvVar, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	fmt.Printf(body)
-
 	env_vars := []EnvVar{}
 	err = json.Unmarshal(body, &env_vars)
 	if err != nil {
