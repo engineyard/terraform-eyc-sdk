57700b8641241e6cf7d0b52709970f553f34b680
Kyle-Law <kylelaw1121@gmail.com>
2022-06-22 13:40:02 +0800

    Printing variables for troubleshooting

--- env_vars.go
+++ env_vars.go
@@ -9,16 +9,21 @@ import (
 // GetEnvVars - /environments - Returns list of environment_variables under the account
 func (c *Client) GetEnvVars() ([]EnvVar, error) {
 	fullURL := fmt.Sprintf("%s/environment_variables", c.HostURL)
+
+	fmt.Printf("fullURL: %v\n", fullURL)
 	req, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
 		return nil, err
 	}
+	fmt.Printf("req: %v\n", req)
 
 	body, err := c.doRequest(req, nil)
+	fmt.Printf("body: %v\n", body)
 
 	env_vars := []EnvVar{}
 
 	err = json.Unmarshal(body, &env_vars)
+	fmt.Printf("envVars: %v\n", env_vars)
 	if err != nil {
 		return nil, err
 	}
