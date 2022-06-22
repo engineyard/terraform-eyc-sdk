e5c4db1e9e022d1ef2ba4f442f69568a3c3ca780
Kyle-Law <kylelaw1121@gmail.com>
2022-06-22 20:53:27 +0800

    Refine env_vars interface

--- env_vars.go
+++ env_vars.go
@@ -9,7 +9,8 @@ import (
 // GetEnvVars - /environments - Returns list of environment_variables under the account
 func (c *Client) GetEnvVars() (EnvVars, error) {
 	fullURL := fmt.Sprintf("%s/environment_variables", c.HostURL)
-	env_vars := EnvVars{}
+	// env_vars := EnvVars{}
+	var env_vars map[string]interface{}
 
 	fmt.Printf("fullURL: %v\n", fullURL)
 	req, err := http.NewRequest("GET", fullURL, nil)
@@ -22,7 +23,7 @@ func (c *Client) GetEnvVars() (EnvVars, error) {
 
 	fmt.Printf("body from GetEnvVars: %v\n", body)
 
-	err = json.Unmarshal(string(body), &env_vars)
+	err = json.Unmarshal(body, &env_vars)
 	fmt.Printf("envVars: %v\n", env_vars)
 	if err != nil {
 		return env_vars, err
