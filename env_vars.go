3fb2705b7db4d27af39979201a73f20e9b78c168
Kyle-Law <kylelaw1121@gmail.com>
2022-07-12 17:07:04 +0800

    Add CreateEnvVar method

--- env_vars.go
+++ env_vars.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // GetEnvVars - /environments - Returns list of environment_variables under the account
@@ -56,4 +57,28 @@ func (c *Client) GetEnvVarsByEnv(env_id int) (map[string]interface{}, error) {
 	}
 
 	return env_vars, nil
+}
+
+// CreateEnvVar - POST /environment_variables
+func (c *Client) CreateEnvVar(envVarParam EnvVarParam) (map[string]interface{}, error) {
+	rb, err := json.Marshal(envVarParam)
+
+	fullURL := fmt.Sprintf("%s/environment_variables", c.HostURL)
+
+	req, err := http.NewRequest("POST", fullURL, strings.NewReader(string(rb)))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ev := EnvVar{}
+	err = json.Unmarshal(body, &ev)
+	if err != nil {
+		return nil, err
+	}
+	return &ev, nil
 }
