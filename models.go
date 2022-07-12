d3249ec5e0e0b78ddafe4c5de5dddaf3ed781c6d
Kyle-Law <kylelaw1121@gmail.com>
2022-07-12 17:06:16 +0800

    Add EnvVarParam & KeyValue struct

--- models.go
+++ models.go
@@ -12,6 +12,17 @@ type EnvVar struct {
 	value            string `json:"value"`
 }
 
+type EnvVarParam struct {
+	environment_variable EnvVarNameValue `json:"environment_variable`
+	application_id       int             `json:"application_id`
+	environment_id       int             `json:"environment_id`
+}
+
+type EnvVarNameValue struct {
+	name  string `json:"name"`
+	value string `json:"value"`
+}
+
 type EnvVars struct {
 	environment_variables []EnvVar `json:"environment_variables"`
 }
